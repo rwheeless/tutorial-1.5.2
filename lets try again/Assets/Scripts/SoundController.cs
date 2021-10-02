585deade1014101bf30261afb5f8dbd849c3d421
Regan Wheeless <regan.wheeless@gmail.com>
2021-10-02 13:24:53 -0400

    code n animation

--- lets try again/Assets/Scripts/SoundController.cs
+++ lets try again/Assets/Scripts/SoundController.cs
@@ -11,29 +11,39 @@ public AudioClip musicClipTwo;
 
 public AudioSource musicSource;
 
+Animator anim;
+
+void Start()
+{
+  anim = GetComponent<Animator>();
+}
+
 void Update()
 {
      if (Input.GetKeyDown(KeyCode.W))
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
+          anim.SetInteger("State", 1);
         }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
+          anim.SetInteger("State", 0);
         }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
         }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
         }
 
      if (Input.GetKeyDown(KeyCode.L))
